e0f65d5d2cac2c8f0cd010592c35fa36019431be
katwold <61921125+katwold@users.noreply.github.com>
2020-08-23 21:06:55 +0200

    Update level-1.js

--- level-1.js
+++ level-1.js
@@ -1,11 +1,16 @@
 // question 1
+
 var city = "oslo"
+
 // question 2
+
 var person = { 
     name: "katarina",
     country: "norway"
 }
+
 // question 3
+
 var outOfStock = true;
 if (outOfStock === true){ 
     console.log("Out of stock");
@@ -13,22 +18,30 @@ if (outOfStock === true){
 else { 
     console.log("In stock");
 }
+
 // question 4
+
 var numbers = [ 5, 10, 15, 20, 25 ];
 for (var i=0; i < numbers.length; i++){
     console.log(numbers[i]);
 } 
+
 // question 5
+
 for (var i = 15; i<=25; i++){
     console.log(i);
 }
+
 // question 6
+
 for (var i = 15; i<=25; i++){
     if (i===20){ 
         console.log(i);
     }
 }
+
 // question 7
+
 var sandals = {
     brand: "Birkenstock",
     size: 39,  
@@ -46,17 +59,23 @@ for (var i=0; i < shoes.length; i++){
     console.log(shoes[i].size);
     console.log(shoes[i].instock);
 }
+
 // question 8
+
 function whatIDontLike(item) {
     console.log("i dont like "+item);
 }
 whatIDontLike("snow");
+
 // question 9
+
 function substraction(numberOne, numberTwo) {
     console.log(numberTwo-numberOne);
 }
 substraction(200, 500);
+
 // question 10
+
 var pets=[];
 function addToArrayPets(item) {
     pets.push(item)
